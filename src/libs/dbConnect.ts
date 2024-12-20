24757f07863dcae594eba3559728124550a73f40
ashwaniKrOm <honey02112001@gmail.com>
2024-12-20 12:04:31 +0530

    new commit

--- src/libs/dbConnect.ts
+++ src/libs/dbConnect.ts
@@ -1,8 +1,13 @@
 import mongoose from "mongoose"
 
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("MongoDB URI is not defined in environment variables");
+}
 export const dbConnect = async ()=>{
     try {
-        await mongoose.connect("mongodb+srv://honey02112001:zLE9Lxjw0L4C9lV1@clustercontactlist.13wss.mongodb.net/contactListDB?retryWrites=true&w=majority&appName=ClusterContactList");
+        await mongoose.connect(uri);
         console.log("db connected successfully");
     } catch (error) {
         console.log("Error in DB connection",error);
